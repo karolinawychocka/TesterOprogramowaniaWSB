c214d699fc5b100206c696d29d7b813a09ceef09
Karolina Wychocka <92202645+karolinawychocka@users.noreply.github.com>
2022-01-27 12:33:57 +0100

    Delete multiplication table.py

--- Nauka_Pythona/multiplication table.py
+++
@@ -1,9 +0,0 @@
-# multiplication table
-
-rows = int(input("How many rows?: "))
-columns = int(input("How many columns?: "))
-
-for i in range(rows):
-    for j in range(columns):
-        print((i + 1) * (j + 1), end=" ")
-    print()
